5fab7c5d044f2ebe367ef5431b517db5041f5823
André Almeida <andre.almeida@escolar.ifrn.edu.br>
2025-09-25 17:17:09 +0000

    Implementação da tabuada.

--- projsala/imc/urls.py
+++ projsala/imc/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns=[
     path('',views.index,name='index'),
     path('nome/',views.nome,name='nome'),
+    path('tabuada/tres/',views.tabuada_tres,name='tabuada_tres'),
 ]
--- projsala/imc/views.py
+++ projsala/imc/views.py
@@ -6,3 +6,9 @@ def index(request):
     return HttpResponse("<h1> Bem-vindo(a) à aplicação IMC</h1>")
 def nome(request):
     return HttpResponse("<h1>André Gustavo Duarte</h1>")
+def tabuada_tres(request):
+    html='<table border="1"><tr><td>Tabuada de três</td></tr>'
+    for i in range(1,11):
+        html+=f'<tr><td>{i} x 3 = {i*3}</td></tr>'
+    html+='</table>'
+    return HttpResponse(html)
