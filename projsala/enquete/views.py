2efe32827361ba8bbc31dd26155dc7b4e3ab7bcc
André Almeida <andre.almeida@escolar.ifrn.edu.br>
2025-10-02 16:44:15 +0000

    Início da implementação da aplicação de enquetes.

--- projsala/enquete/views.py
+++ projsala/enquete/views.py
@@ -2,5 +2,22 @@ from django.shortcuts import render
 
 # Create your views here.
 
+pergunta="Qual seu animal favorito?"
+alternativa1="Cachorro"
+alternativa2="Gato"
+alternativa3="Pássaro"
+alternativa4="Peixe"
+voto1=0
+voto2=0
+voto3=0
+voto4=0
 def home(request):
-    return render(request,'home.html')
+    contexto={'pergunta':pergunta,
+              'alternativa1':alternativa1,
+              'alternativa2':alternativa2,
+              'alternativa3':alternativa3,
+              'alternativa4':alternativa4}
+    return render(request,'home.html',contexto)
+def votar(request):
+    #Lógica para contabilizar votos
+    render(request,'resultado.html')
