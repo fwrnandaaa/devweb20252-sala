c746e5703249ce2c453ad4768840030982ff7664
Fernanda Dantas do Nascimento <110307073+fwrnandaaa@users.noreply.github.com>
2025-11-05 20:52:22 +0000

    Criação das primeiras raças e gatinhos no bancpo de dados utilizando o shell

--- projadocato/adocato/models.py
+++ projadocato/adocato/models.py
@@ -3,6 +3,9 @@ from django.db import models
 # Create your models here.
 class Raca(models.Model): #fazendo herança a partir da classe model
     nome=models.CharField(max_length=100,unique=True) #models.charfield= chama construtor e passa 2 parametros: max_length e unique; o que quer dizer que só é possivel cadastrar uma raça com no max 100 caracteres e ele deve ser unique
+    def __str__(self):
+        return self.nome
+        #para mostrar o nome da raça 
 
 class Gato(models.Model):
     #max_length define o tamanho maximo do atributo
@@ -15,4 +18,6 @@ class Gato(models.Model):
     data_nascimento=models.DateField()
     descricao=models.TextField(blank=True,null=True) #esses dois parametros juntos deixam claro que o campo 'descricao' é opcional
     disponivel=models.BooleanField(default=True) #defalt = True significa que assim que o gato for cadastrado ele é marcado como disponivel
-    raca=models.ForeignKey(Raca,on_delete=models.CASCADE,related_name='gatos') #atributo 'raca' é chave estrangeira da classe Raca; related_name injeta atributo 'gatos' na classe Raca, oque facilita pesquisa por filtragem
+    raca=models.ForeignKey(Raca,on_delete=models.CASCADE,related_name='gatos') # atributo 'raca' é chave estrangeira da classe Raca;
+    # on_delete=models.CASCADE = ao deletar uma raça todos os gartos relacionados a essa raça serao deletados; 
+    # related_name injeta atributo 'gatos' na classe Raca, oque facilita pesquisa por filtragem
